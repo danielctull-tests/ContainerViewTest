3aff67518b6edd0b13c0b9e8eaa555712fab3267
Daniel Tull <dt@danieltull.co.uk>
2013-10-08 13:13:29 +0100

    Initial Commit

---
+++ ContainerViewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContainerViewTest
+//
+//  Created by Daniel Tull on 08.10.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
